cfdc64b3a5b056147b727b0c977faee5b14cbeea
Ranjith THOTA <ranjith.thota@gxpmanager.com>
2023-12-11 22:04:43 +0100

    height fix

--- src/Components/Content.tsx
+++ src/Components/Content.tsx
@@ -8,7 +8,7 @@ export default function Content({ isMenuOpen }: any) {
   const [isDoubleScreen, setisDoubleScreen] = useState(true);
   return (
     <div
-      className=" h-full p-[1rem] transition-width ease-in-out duration-100 shadow-lg "
+      className="h-full p-[1rem] transition-width ease-in-out duration-100 shadow-lg "
       style={{ width: isMenuOpen ? "calc(100% - 336px)" : "calc(100% - 48px)" }}
     >
       <button
@@ -21,7 +21,7 @@ export default function Content({ isMenuOpen }: any) {
           <img src={open} alt="" className=" w-[2rem] h-[2rem]" />
         )}
       </button>
-      <div className="w-full h-full h-[calc(100vh-64px-5rem)] mt-[1rem] mt-b-[1rem] ">
+      <div className="w-full h-[calc(100%-3rem)] mt-[1rem]">
         <Screen1 isDoubleScreen={isDoubleScreen} />
         <Screen2 isDoubleScreen={isDoubleScreen} />
       </div>
